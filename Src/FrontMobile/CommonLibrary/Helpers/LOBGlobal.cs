94e1d12ba533baeb0b20a3605fde944335d77289
Vulcan <vulcan.lee@gmail.com>
2020-11-30 22:19:00 +0800

    BaseWebAPI

---
+++ Src/FrontMobile/CommonLibrary/Helpers/LOBGlobal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLibrary.Helpers
+{
+    public class LOBGlobal
+    {
+        public static string APIHost = "";
+        public static string JSONDataKeyName = "";
+    }
+}
