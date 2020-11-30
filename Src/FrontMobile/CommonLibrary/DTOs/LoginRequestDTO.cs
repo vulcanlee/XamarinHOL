371b8ae91f6c1a832246117f61cd2a59629c190f
Vulcan <vulcan.lee@gmail.com>
2020-11-30 16:25:49 +0800

    Create Dto

---
+++ Src/FrontMobile/CommonLibrary/DTOs/LoginRequestDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLibrary.DTOs
+{
+    public class LoginRequestDTO
+    {
+        public string Account { get; set; }
+        public string Password { get; set; }
+    }
+}
